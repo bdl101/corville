3249a91dea17803b81d33abecb2318ed25c8d842
bdl101 <blake.lipson.dev@gmail.com>
2026-09-05 12:04:07 -0400

    implement spec 2 - design system

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import './styles/global.css'
+import './styles/tokens.css'
+import './styles/base.css'
+import './styles/components.css'
+import './styles/layout.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = '<p>Corville</p>'
